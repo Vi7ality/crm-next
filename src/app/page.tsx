b7a2a66eccdf96eb2c1705ecab10580b92caa159
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-05-28 22:30:18 +0300

    tailwind init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLabel, { Status } from './components/status-label';
+
 export default function Home() {
   return (
-    <main>
+    <main className="text-2xl">
       <h1>Home</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
