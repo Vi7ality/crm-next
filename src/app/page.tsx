ed4f8d49643e0a297e9c0f8a60695d6662101381
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-09 17:43:11 +0300

    env test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import StatusLabel, { Status } from '@/app/components/status-label';
 
 export default function Home() {
+  console.log(process.env.CRM_SECRET_KEY)
   return (
     <main>
       <h1 className="text-2xl">Home page</h1>
