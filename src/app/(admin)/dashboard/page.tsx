7125ddd958dbc2e02ac9e472995628c2aa4f7e37
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-10 20:11:02 +0300

    general layout added

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/app/components/header';
+
+interface DashboardPageProps {}
+
+const Page = ({}: DashboardPageProps) => {
+  return (
+    <>
+      <Header>Dashboard page</Header>
+    </>
+  );
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,6 @@ export default function Home() {
     <main>
       <h1 className="text-2xl">Home page</h1>
       <AddCompanyButton />
-      {/* <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel> */}
     </main>
   );
 }
