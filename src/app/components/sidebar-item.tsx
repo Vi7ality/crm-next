e46f284ec8492655db98936f118517ea127f14d7
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-10 21:46:49 +0300

    exit navigation added

--- src/app/components/sidebar-item.tsx
+++ src/app/components/sidebar-item.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export interface SidebarItemProps {
   pathname: string;
@@ -11,10 +12,10 @@ export interface SidebarItemProps {
 const SidebarItem = ({ pathname, src, alt, children }: SidebarItemProps) => {
   return (
     <li>
-      <a href={pathname} className="flex items-center h-9 mx-1 gap-3.5">
+      <Link href={pathname} className="flex items-center h-9 mx-1 gap-3.5">
         <Image className="ml-5" width={18} height={18} src={src} alt={alt} />
         <span className="font-medium text-zinc-50">{children}</span>
-      </a>
+      </Link>
     </li>
   );
 };
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,10 +1,19 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import SidebarItem from '@/app/components/sidebar-item';
+import { useRouter } from 'next/navigation';
 
 export interface SidebarProps {}
 
 const Sidebar = ({}: SidebarProps) => {
+  const router = useRouter();
+
+  const handleExitClick = () => {
+    router.push('/');
+  };
+
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
@@ -38,7 +47,12 @@ const Sidebar = ({}: SidebarProps) => {
             src="/icons/arrow-left-on-rectangle.svg"
             alt="exit icon"
           />
-          <span className="font-medium text-white">Exit</span>
+          <span
+            onClick={() => handleExitClick()}
+            className="font-medium text-white"
+          >
+            Exit
+          </span>
         </button>
       </div>
     </aside>
