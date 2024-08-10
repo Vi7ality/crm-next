7f6a945133747b87eacd35c874ddae36e539407b
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-10 14:31:15 +0300

    Input Field added

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,20 +1,22 @@
-'use client'
+'use client';
 
-import clsx from "clsx";
+import clsx from 'clsx';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    disabled?: boolean;
+  disabled?: boolean;
 }
 
-const Button = ({disabled, ...rest}: Props) => {
-    return ( 
-        <button {...rest} className={clsx('py-2.5 px-5 bg-gray-900 text-zinc-50 text-base text-center font-medium rounded',
-            !disabled && 'hover:bg-gray-800 active:bg-gray-950',
-            disabled && 'text-zinc-100'
-        )}>
-            
-        </button>
-    )
-}
+const Button = ({ disabled, ...rest }: Props) => {
+  return (
+    <button
+      {...rest}
+      className={clsx(
+        'py-2.5 px-5 bg-gray-900 text-zinc-50 text-base text-center font-medium rounded',
+        !disabled && 'hover:bg-gray-800 active:bg-gray-950',
+        disabled && 'text-zinc-100',
+      )}
+    ></button>
+  );
+};
 
 export default Button;
