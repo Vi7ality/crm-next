864a47c9e77115528ee1c678846b11303b76b9ec
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-12 14:33:56 +0300

    api added to pages

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -58,3 +58,78 @@ const PROJECT_TOKEN = process.env.NEXT_PUBLIC_PROJECT_TOKEN;
 
 const buildUrl = (...paths: string[]) =>
   `https://${PROJECT_TOKEN}.mockapi.io/api/v1/${paths.join('/')}`;
+
+const stringifyQueryParams = (params: Record<string, string>) =>
+  new URLSearchParams(params).toString();
+
+const sendRequest = async <T>(url: string, init?: RequestInit) => {
+  const res = await fetch(url, init);
+  if (!res.ok) {
+    throw new Error(await res.text());
+  }
+
+  return (await res.json()) as T;
+};
+
+export const getSummaryStats = (init?: RequestInit) => {
+  return sendRequest<SummaryStats>(buildUrl('summary-stats', '1'), init);
+};
+
+export const getSummarySales = (init?: RequestInit) => {
+  return sendRequest<SummarySales[]>(buildUrl('summary-sales'), init);
+};
+
+export const getCountries = (init?: RequestInit) => {
+  return sendRequest<Country[]>(buildUrl('countries'), init);
+};
+
+export const getCategories = (init?: RequestInit) => {
+  return sendRequest<Category[]>(buildUrl('categories'), init);
+};
+
+export const getCompanies = (init?: RequestInit) => {
+  return sendRequest<Company[]>(buildUrl('companies'), init);
+};
+
+export const getCompany = (id: string, init?: RequestInit) => {
+  return sendRequest<Company>(buildUrl('companies', id), init);
+};
+
+export const getPromotions = async (
+  params: Record<string, string> = {},
+  init?: RequestInit,
+) => {
+  return sendRequest<Promotion[]>(
+    `${buildUrl('promotions')}?${stringifyQueryParams(params)}`,
+    init,
+  );
+};
+
+export const createCompany = async (
+  data: Omit<Company, 'id' | 'hasPromotions'>,
+  init?: RequestInit,
+) => {
+  return sendRequest<Company>(buildUrl('companies'), {
+    ...init,
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      ...(init && init.headers),
+      'content-type': 'application/json',
+    },
+  });
+};
+
+export const createPromotion = async (
+  data: Omit<Promotion, 'id'>,
+  init?: RequestInit,
+) => {
+  return sendRequest<Promotion>(buildUrl('promotions'), {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      ...(init && init.headers),
+      'content-type': 'application/json',
+    },
+  });
+};
