aed9895ba2b9e20a4860be7c3f483a336d3807b2
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-13 15:06:58 +0300

    console cleared

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import StatusLabel, { Status } from '@/app/components/status-label';
 import AddCompanyButton from './components/add-company-button';
 
 export default function Home() {
-  console.log(process.env.CRM_SECRET_KEY);
   return (
     <main>
       <h1 className="text-2xl">Home page</h1>
